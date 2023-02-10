3ef773b43c7d437473c67c1dc8db6032cd3151de
Mohan-53 <124578566+Mohan-53@users.noreply.github.com>
2023-02-11 01:05:45 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Parent }   from './Parent'
+
+export default class 
+ extends Component {
+  render() {
+    return (
+      <div>
+        <Parent />
+      </div>
+    )
+  }
+}
+
